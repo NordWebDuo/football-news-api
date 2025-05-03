f82a5eb46ac105143d0f947c01d3c730e8f14c29
NordWebDuo <contact@nordwebduo.com>
2025-05-03 13:33:02 +0000

    change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const router = express.Router();
 
 // Arrays pentru fiecare sursă
-tconst news_array_ninenine = [];
+const news_array_ninenine = [];
 const news_array_onefootball = [];
 const news_array_espn = [];
 const news_array_goaldotcom = [];
